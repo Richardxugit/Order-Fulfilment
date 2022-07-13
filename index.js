4e33322732bd99f84c2f128a1e5e94f0dafb3bc0
Richardxugit <cansolion@163.com>
2022-07-13 16:15:30 +1000

    feat: Add update core logic to entry method

--- index.js
+++ index.js
@@ -7,15 +7,33 @@ const fulfillment = new Fulfillment();
 const inventory = new Inventory();
 
 const processOrders = () => {
-  console.log("Welcome to the sample order fulfillment application!\n");
+  console.log("##Welcome to the sample order fulfillment application!##\n");
   const orders = data.orders;
   const products = data.products;
 
   const orderIds = fulfillment.inputOrderId();
   const processOrders = fulfillment.checkOrders(orderIds, orders);
-  fulfillment.orderFulfillment(processOrders, products)
-  console.log(processOrders)
+  const {
+    allProducts: updatedProducts,
+    fulfilledOrderIds,
+    unFulfilledOrderIds,
+  } = fulfillment.orderFulfillment(processOrders, products);
+  if (fulfilledOrderIds.length > 0) {
+    console.log(
+      `\nOrder ID(s): ${fulfilledOrderIds.map((id) => {
+        return id;
+      })} has been fulfilled`
+    );
+    inventory.createShipping(fulfilledOrderIds);
+  }
+  if (unFulfilledOrderIds.length > 0) {
+    console.log(
+      `\nOrder ID(s): ${unFulfilledOrderIds.map((id) => {
+        return id;
+      })} can not be fulfilled`
+    );
+  }
+  inventory.checkReorderThreshold(updatedProducts);
 };
-
 
 processOrders();
